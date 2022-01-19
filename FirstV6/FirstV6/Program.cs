34feb190b912a3404c3f8f5d028a9ce0c591e20d
Jaime J Cobo <jaimecobo@outlook.com>
2022-01-19 17:26:47 -0400

    FirstV6

---
+++ FirstV6/FirstV6/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(args.Length);
+// Press Ctrl + F5 to run
+
+int i = 10;
+Console.WriteLine(i);
+string name = "Lucas";
+
+Console.WriteLine($"Hello, {name}!");
