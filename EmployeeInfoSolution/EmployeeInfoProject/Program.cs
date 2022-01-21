a19d5919d002abac01b473ede846929188a609f6
Jaime J Cobo <jaimecobo@outlook.com>
2022-01-21 00:41:25 -0400

    EmployeeInfo

---
+++ EmployeeInfoSolution/EmployeeInfoProject/Program.cs
@@ -0,0 +1,17 @@
+﻿
+/*
+ * You have just hired a new employee named: Lucinda Potter, who was born on the 24th of June in 1992. Their work phone is: 312-982-1010 and their email is ewoz@woz-u.com.
+ */
+
+
+    string fullName = "Lucinda Potter";
+    string birthdate = "24th of June in 1992";
+    string phoneNumber = "312-982-1010";
+    string email = "ewoz@woz-u.com";
+
+
+
+Console.WriteLine(fullName);
+Console.WriteLine(birthdate);
+Console.WriteLine(phoneNumber[8] + "" + phoneNumber[9] + "" + phoneNumber[10] + "" + phoneNumber[11]);
+Console.WriteLine(email);
