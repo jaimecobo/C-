c4c205cceb43a6ec23504c3ed789b2996b62633c
Jaime J Cobo <jaimecobo@outlook.com>
2022-01-22 08:52:45 -0400

    EmployeePractice

---
+++ EmployeePractice/EmployeePractice/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter the First Integer: ");
+int num1 =  Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the Second Integer: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The sum of {num1} + {num2} = {num1+num2}");
+
