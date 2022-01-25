88c13984e8badbadf5804520acabc74fb7aedace
Jaime J Cobo <jaimecobo@outlook.com>
2022-01-24 23:43:38 -0400

    Hypotenuse

---
+++ Hypotenuse/Hypotenuse/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Hypotenuse
+{
+    public class StudentProgram
+    {
+        public static void Main()
+        {
+            double x = Hypotenuse(10.0, 20.0);
+            Console.WriteLine(x);
+        }
+        static double Hypotenuse(double a, double b)
+        {
+            return Math.Sqrt(a * a + b * b);
+        }
+    }
+}
