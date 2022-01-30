cb1fd2d725bd9ef3b723dfd470d8f215f2c5b5a2
Jaime J Cobo <jaimecobo@outlook.com>
2022-01-30 13:24:54 -0400

    C# FinalProject 002-01/30/2022

---
+++ FinalProject/FinalProject/Part2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2
+{
+
+
+    class EmployeeRecord
+    {
+        // create an employee Record with public properties
+        //    ID                        a number to identify an employee
+        //    Name                      the employee name
+        //    StateCode                 the state collecting taxes for this employee
+        //    HoursWorkedInTheYear      the total number of hours worked in the entire year (including fractions of an hour)
+        //    HourlyRate                the rate the employee is paid for each hour worked
+        //                                  assume no changes to the rate throughout the year.
+
+        //    provide a constructor that takes a csv and initializes the employeerecord
+        //       do all error checking and throw appropriate exceptions
+
+        //    provide an additional READ ONLY property called  YearlyPay that will compute the total income for the employee
+        //        by multiplying their hours worked by their hourly rate
+
+        //    provide an additional READONLY property that will compute the total tax due by:
+        //        calling into the taxcalculator providing the statecode and the yearly income computed in the YearlyPay property
+
+        //    provide an override of toString to output the record : including the YearlyPay and the TaxDue
+    }
+
+    static class EmployeesList
+    {
+
+        // create an EmployeeList class that will read all the employees form the Employees.csv file
+        // the logic is similar to the way the taxcalculator read its taxrecords
+
+        // Create a List of employee records.  The employees are arranged into a LIST not a DICTIONARY
+        //   because we are not accessing the employees by state,  we are accessing the employees sequentially as a list
+
+        // create a static constructor to load the list from the file
+        //   be sure to include try/catch to display messages
+
+
+    }
+
+
+
+
+    class Program
+    {
+
+        // loop over all the employees in the EmployeeList and print them
+        // If the ToString() in the employee record is correct, all the data will print out.
+
+        public static void MainPart2()
+        {
+
+            try
+            {
+
+                // write your logic here
+            }
+
+
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
--- FinalProject/FinalProject/Program.cs
+++ FinalProject/FinalProject/Program.cs
@@ -66,161 +66,177 @@ Dictionary<String, List<string>> dic = new Dictionary<string, List<string>>();
 */
 
 using System.Linq;
-public class TaxRecord
-{
-    public string statecode;
-    public string statename;
-    public decimal floorRate;
-    public decimal ceilingRate;
-    public decimal taxRate;
-   
-    public TaxRecord() { }
-    public TaxRecord(string csv)
+//namespace Final_Project
+//{
+    public class TaxRecord
     {
-        string[] data = csv.Split(',');
-        if (data.Length != 5)
+        public string statecode;
+        public string statename;
+        public decimal floorRate;
+        public decimal ceilingRate;
+        public decimal taxRate;
+
+        public TaxRecord() { }
+        public TaxRecord(string csv)
         {
-            throw new Exception("Each line must have 'State Code', 'State Name', 'Floor Rate', 'Ceiling Rate' and 'Tax Rate'. \n" +
-                                $"This line contains the following data: {csv} \n");
+            string[] data = csv.Split(',');
+            if (data.Length != 5)
+            {
+                throw new Exception("Each line must have 'State Code', 'State Name', 'Floor Rate', 'Ceiling Rate' and 'Tax Rate'. \n" +
+                                    $"This line contains the following data: {csv} \n");
+            }
+            if (data[0].Length != 2)
+            {
+                throw new Exception($"Invalid state code, the state code received is {data[0]}, state codes must be form by 2 capital letters \n");  //The state code is not equal to two.
+            }
+            this.statecode = data[0];
+            this.statename = data[1];
+            decimal toCarryTryParseDecimal;
+            if (!decimal.TryParse(data[2], out toCarryTryParseDecimal))
+            {
+                throw new Exception($"The floor rate is not recognizable as decimal. -- Impossible to parse '{data[2]}' to decimal --\n");
+            }
+            this.floorRate = toCarryTryParseDecimal;
+            if (!decimal.TryParse(data[3], out toCarryTryParseDecimal))
+            {
+                throw new Exception($"The ceiling rate is not recognizable as decimal. -- Impossible to parse '{data[3]}' to decimal --\n");
+            }
+            this.ceilingRate = toCarryTryParseDecimal;
+            if (!decimal.TryParse(data[4], out toCarryTryParseDecimal))
+            {
+                throw new Exception($"The tax rate is not recognizable as decimal. -- Impossible to parse '{data[4]}' to decimal --\n");
+            }
+            this.taxRate = toCarryTryParseDecimal;
+
         }
-        if(data[0].Length != 2)
+
+        public override string ToString()
         {
-            throw new Exception($"Invalid state code, the state code received is {data[0]}, state codes must be form by 2 capital letters \n");  //The state code is not equal to two.
+            return $"{statecode,4} {statename,15} {floorRate,10} {ceilingRate,10} {taxRate}";
         }
-        this.statecode = data[0];
-        this.statename = data[1];
-        decimal toCarryTryParseDecimal;
-        if (!decimal.TryParse(data[2], out toCarryTryParseDecimal))
-        {
-            throw new Exception($"The floor rate is not recognizable as decimal. -- Impossible to parse '{data[2]}' to decimal --\n");
-        }
-        this.floorRate = toCarryTryParseDecimal;
-        if (!decimal.TryParse(data[3], out toCarryTryParseDecimal))
-        {
-            throw new Exception($"The ceiling rate is not recognizable as decimal. -- Impossible to parse '{data[3]}' to decimal --\n");
-        }
-        this.ceilingRate = toCarryTryParseDecimal;
-        if(!decimal.TryParse(data[4], out toCarryTryParseDecimal))
-        {
-            throw new Exception($"The tax rate is not recognizable as decimal. -- Impossible to parse '{data[4]}' to decimal --\n");
-        }
-        this.taxRate = toCarryTryParseDecimal;
+
+
+
 
     }
 
-    public override string ToString()
+    class taxCalculator
     {
-        return $"{statecode, 4} {statename, 15} {floorRate, 10} {ceilingRate, 10} {taxRate}";
-    }
-    
-
-    
-
-}
-
-class taxCalculator
-{
-    static Dictionary<string, List<TaxRecord>> taxRecords = new Dictionary<string, List<TaxRecord>>();
-    static taxCalculator()
-    {
-
-        var reader = File.OpenText(@"taxtable.csv");
-        string line;
-        do
+        static Dictionary<string, List<TaxRecord>> taxRecords = new Dictionary<string, List<TaxRecord>>();
+        static taxCalculator()
         {
-            try
-            {
-                line = reader.ReadLine();
-                TaxRecord r = new TaxRecord(line);
-                Console.WriteLine(r);
-                if (taxRecords.ContainsKey(r.statecode))
-                {
-                    taxRecords[r.statecode].Add(r);
-                }
-                else
-                {
-                    List<TaxRecord> list = new List<TaxRecord>();
-                    list.Add(r);
-                    taxRecords.Add(r.statecode, list);
-                }
 
-            }
-            catch (Exception ex)
+            var reader = File.OpenText(@"taxtable.csv");
+            string line;
+            do
             {
-                Console.WriteLine(ex.ToString());
-            }
-        }
-        while (!reader.EndOfStream);
-        reader.Close();
-    }
-
-    public decimal ComputeTaxFor(string stateAbv, decimal incomeEarned, bool verbose)
-    {
-        if (verbose)
-        {
-            Console.WriteLine($"  ---------------------------------------------------------------------------------------------------------------------------");
-            Console.WriteLine($" {"| Your total income is = ",25} {incomeEarned, 12}{"                                                    |", 87}");  
-            Console.WriteLine(" |---------------------------------------------------------------------------------------------------------------------------|"); 
-            Console.WriteLine($"{" |       INCOME", 12} {" -", 4} {"FLOOR RATE", 12} {"=", 4} {"TAXABLE", 12} {"*", 3} {"  TAX RATE  ", 8} {"=", 1} {"    TAX DUE  |", 55}");
-            Console.WriteLine(" |---------------------------------------------------------------------------------------------------------------------------|");
-        }
-        decimal finalComputedTax = 0m;
-
-        foreach (string statecode in taxRecords.Keys)
-        {
-            if (!taxRecords.ContainsKey(stateAbv))
-            {
-                throw new Exception($"There is no record for state '{stateAbv}' \n");
-            }
-            foreach (TaxRecord r in taxRecords[statecode])
-            {
-                if (r.statecode == stateAbv)
+                try
                 {
-                    //Console.WriteLine($"{r.statecode}");
-                    //Console.WriteLine($"{r.statecode} - {r.floorRate,10} - {r.ceilingRate,10} - {r.taxRate, 10}");
-                    if(incomeEarned >= r.ceilingRate)
+                    line = reader.ReadLine();
+                    TaxRecord r = new TaxRecord(line);
+                    Console.WriteLine(r);
+                    if (taxRecords.ContainsKey(r.statecode))
                     {
-                        finalComputedTax += (r.ceilingRate - r.floorRate) * r.taxRate;
-                        if (verbose)
-                        {
-                            Console.WriteLine($" | {r.ceilingRate, 12} {"-", 4} {r.floorRate, 12} {"=", 4} {r.ceilingRate - r.floorRate, 12} {" * ", 4} " +
-                                $"{r.taxRate, 8} {"=",4} {"  tax due for this income portion =", 36} {finalComputedTax, 15}  |");
-                        }
-                        continue;
+                        taxRecords[r.statecode].Add(r);
+                    }
+                    else
+                    {
+                        List<TaxRecord> list = new List<TaxRecord>();
+                        list.Add(r);
+                        taxRecords.Add(r.statecode, list);
                     }
 
-                    if (incomeEarned > r.floorRate && incomeEarned < r.ceilingRate)
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            while (!reader.EndOfStream);
+            reader.Close();
+        }
+
+        public decimal ComputeTaxFor(string stateAbv, decimal incomeEarned, bool verbose)
+        {
+            if (verbose)
+            {
+            //Console.WriteLine($"  ---------------------------------------------------------------------------------------------------------------------------");
+            //Console.WriteLine($" {"| Your total income is = ",25} {incomeEarned,12}{"                                                    |",87}");
+            //Console.WriteLine(" |---------------------------------------------------------------------------------------------------------------------------|");
+            //Console.WriteLine($"{" |       INCOME",12} {" -",4} {"FLOOR RATE",12} {"=",4} {"TAXABLE",12} {"*",3} {"  TAX RATE  ",8} {"=",1} {"    TAX DUE  |",55}");
+            //Console.WriteLine(" |---------------------------------------------------------------------------------------------------------------------------|");
+            Console.WriteLine("\n");
+            Console.WriteLine($"  --------------------------------------------------------------------------------------");
+            Console.WriteLine($" {"| Your total income is = ",25} {incomeEarned,12}{"                                               |",50}");
+            Console.WriteLine(" |--------------------------------------------------------------------------------------|");
+            Console.WriteLine($"{" |       INCOME",12} {" -",4} {"FLOOR RATE",12} {"=",4} {"TAXABLE",12} {"*",3} {"  TAX RATE  ",8} {"=",1} {"    TAX DUE  |",18}");
+            Console.WriteLine(" |--------------------------------------------------------------------------------------|");
+        }
+            decimal finalComputedTax = 0m;
+
+            foreach (string statecode in taxRecords.Keys)
+            {
+                if (!taxRecords.ContainsKey(stateAbv))
+                {
+                    throw new Exception($"There is no record for state '{stateAbv}' \n");
+                }
+                foreach (TaxRecord r in taxRecords[statecode])
+                {
+                    if (r.statecode == stateAbv)
                     {
-                        finalComputedTax += (incomeEarned - r.floorRate) * r.taxRate;
-                        if (verbose)
+                        //Console.WriteLine($"{r.statecode}");
+                        //Console.WriteLine($"{r.statecode} - {r.floorRate,10} - {r.ceilingRate,10} - {r.taxRate, 10}");
+                        if (incomeEarned >= r.ceilingRate)
                         {
+                            finalComputedTax += (r.ceilingRate - r.floorRate) * r.taxRate;
+                            if (verbose)
+                            {
+                                //Console.WriteLine($" | {r.ceilingRate,12} {"-",4} {r.floorRate,12} {"=",4} {r.ceilingRate - r.floorRate,12} {" * ",4} " +
+                                //    $"{r.taxRate,8} {"=",4} {"  tax due for this income portion =",36} {finalComputedTax,15}  |");
+                                Console.WriteLine($" | {r.ceilingRate,12} {"-",4} {r.floorRate,12} {"=",4} {r.ceilingRate - r.floorRate,12} {" * ",4} " +
+                                    $"{r.taxRate,8} {"=",4} {finalComputedTax,15}  |");
+                        }
+                            continue;
+                        }
+
+                        if (incomeEarned > r.floorRate && incomeEarned < r.ceilingRate)
+                        {
+                            finalComputedTax += (incomeEarned - r.floorRate) * r.taxRate;
+                            if (verbose)
+                            {
+                            //Console.WriteLine($" | {incomeEarned,12} {"-",4} {r.floorRate,12} {"=",4} {incomeEarned - r.floorRate,12} {" * ",4} " +
+                            //    $"{r.taxRate,8} {"=",4} {"  tax due for this income portion =",36} {finalComputedTax,15}  |");
                             Console.WriteLine($" | {incomeEarned,12} {"-",4} {r.floorRate,12} {"=",4} {incomeEarned - r.floorRate,12} {" * ",4} " +
-                                $"{r.taxRate,8} {"=",4} {"  tax due for this income portion =",36} {finalComputedTax,15}  |");
+                                $"{r.taxRate,8} {"=",4} {finalComputedTax,15}  |");
 
                         }
+                        }
                     }
+
+
                 }
-
-
             }
+            if (verbose)
+            {
+            //Console.WriteLine(" |---------------------------------------------------------------------------------------------------------------------------|");
+            //Console.WriteLine($" {"|                                                                            TOTAL TAX DUE = ",85} {finalComputedTax,15:0000.00} {"              |",15}");
+            //Console.WriteLine($"{"  ---------------------------------------------------------------------------------------------------------------------------"}");
+            Console.WriteLine(" |--------------------------------------------------------------------------------------|");
+            Console.WriteLine($" {"|                                            TOTAL TAX DUE = ",49} {finalComputedTax,15:0.00} {"         |",9}");
+            Console.WriteLine($"{"  --------------------------------------------------------------------------------------"}");
         }
-        if (verbose)
+            return finalComputedTax;
+        }
+    }
+
+
+    class Program
+    {
+        public static void Main()
         {
-            Console.WriteLine(" |---------------------------------------------------------------------------------------------------------------------------|");
-            Console.WriteLine($" {"|                                                                            TOTAL TAX DUE = ",85} {finalComputedTax,15:0000.00} {"              |",15}");
-            Console.WriteLine($"{"  ---------------------------------------------------------------------------------------------------------------------------"}");
+            taxCalculator taxCalc = new taxCalculator();
+            Console.WriteLine("\n TOTAL TAX DUE = " + taxCalc.ComputeTaxFor("CA", 9, true) + "\n");
         }
-        return finalComputedTax;
     }
-}
 
-
-class Program
-{
-    
-    public static void Main()
-    { 
-        taxCalculator taxCalc = new taxCalculator();
-        Console.WriteLine("taxCalc = " + taxCalc.ComputeTaxFor("CA", 999999, true));
-    }
-}
+//}
